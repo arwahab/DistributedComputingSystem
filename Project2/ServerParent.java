1443541e5af57a5219310b0bb5122284d2d93f18
ryancmcmichael <ryancmcmichael@yahoo.com>
2016-11-10 19:45:56 -0500

    Add files via upload

--- Project2/ServerParent.java
+++ Project2/ServerParent.java
@@ -1,4 +1,7 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,6 +21,16 @@ public class ServerParent implements Runnable {
     }
 
     public void run() {
+        try{
+            Socket routerSocket = new Socket("10.99.9.1", 5555);
+            DataInputStream in = new DataInputStream(routerSocket.getInputStream());
+            DataOutputStream out = new DataOutputStream(routerSocket.getOutputStream());
+            out.writeUTF("SERVER");
+            out.writeUTF(Inet4Address.getLocalHost().getHostAddress());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
         while (true) {
             try {
                 // Create a server socket and start listening
@@ -29,7 +42,8 @@ public class ServerParent implements Runnable {
                 System.out.println(String.format("Connected to %s", socket.getInetAddress()));
 
                 System.out.println("Sending Socket object to new Server thread");
-                (new Thread(new Server(socket))).start();    
+                (new Thread(new Server(socket))).start();
+                serverSocket.close();
             } catch (IOException e) {
                 System.out.println("IOException in ServerParent");
                 System.out.println(e);
