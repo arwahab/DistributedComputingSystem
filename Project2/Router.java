5042f9a34a8bf440a5c42532521d5a6391d078ba
Jun Nguyen <Jun Nguyen>
2016-11-15 19:13:06 -0500

    router modified

--- Project2/Router.java
+++ Project2/Router.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Exchanger;
 import java.util.stream.Collectors;
 
 public class Router {
@@ -31,43 +32,48 @@ public class Router {
     }
 
     public static void main(String[] args) throws IOException {
+
         ServerSocket serverSocket = new ServerSocket(routerPort);
         System.out.println("Router is listening on port: " + routerPort);
 
 
         while (true) {
-            Socket clientSocket = serverSocket.accept();
-            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-            String inputLine = in.readUTF();
-            System.out.println("request type: " + inputLine);
-            String serverIpAddress;
-            int serverPortNumber;
+            try {
+                Socket clientSocket = serverSocket.accept();
+                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+                String inputLine = in.readUTF();
+                System.out.println("request type: " + inputLine);
+                String serverIpAddress;
+                int serverPortNumber;
 
-            switch (inputLine) {
-                case "SERVER":
-                    System.out.println("server online request");
-                    serverIpAddress = in.readUTF();
-                    serverPortNumber = Integer.parseInt(in.readUTF());
-                    addIpAddress(serverIpAddress, serverPortNumber);
-                    System.out.println("IP Address: " + serverIpAddress + " was added to routing table");
-                    break;
-                case "SERVER_OFFLINE":
-                    System.out.println("server offline request");
-                    serverIpAddress = in.readUTF();
-                    removeIpAddress(serverIpAddress);
-                    System.out.println("IP Address: " + serverIpAddress + " was removed from routing table");
-                    break;
-                case "CLIENT":
-                    System.out.println("client request");
-                    IpPort ipPort = retrieveIpAddress();
-                    out.writeUTF(ipPort.getIp());
-                    out.writeUTF(String.valueOf(ipPort.getPort()));
-                    break;
+                switch (inputLine) {
+                    case "SERVER":
+                        System.out.println("server online request");
+                        serverIpAddress = in.readUTF();
+                        serverPortNumber = Integer.parseInt(in.readUTF());
+                        addIpAddress(serverIpAddress, serverPortNumber);
+                        System.out.println("IP Address: " + serverIpAddress + " was added to routing table");
+                        break;
+                    case "SERVER_OFFLINE":
+                        System.out.println("server offline request");
+                        serverIpAddress = in.readUTF();
+                        removeIpAddress(serverIpAddress);
+                        System.out.println("IP Address: " + serverIpAddress + " was removed from routing table");
+                        break;
+                    case "CLIENT":
+                        System.out.println("client request");
+                        IpPort ipPort = retrieveIpAddress();
+                        out.writeUTF(ipPort.getIp());
+                        out.writeUTF(String.valueOf(ipPort.getPort()));
+                        break;
+                }
+                clientSocket.close();
+            }catch(Exception e){
+                e.printStackTrace();
+                System.out.println(e);
             }
-            clientSocket.close();
         }
-
     }
 
     static void addIpAddress(String serverAddress, int serverPort) {
